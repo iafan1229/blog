ecee237eac6133affed005d018c73b33cc9f49c2
iafan1229 <home0@naver.com>
2020-06-21 10:02:24 +0900

    UI 컴포넌트 만들기

---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 회원가입/로그인 페이지 레이아웃
+ */
+
+ const HeaderBlock= styled.div``;
+
+ const Header = () => {
+     return (
+         <HeaderBlock>
+
+         </HeaderBlock>
+     );
+ };
+
+ export default Header;
